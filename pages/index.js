df0d864678094eb75332e7eede4ef2cfff81f9bf
Magdalena Ortega Solís <magdalena.ortegasolis@gmail.com>
2022-11-26 14:27:19 -0300

    navbar y footer

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className ={styles.container}>
+
+      <Head>
+        <title>Dolce&Vegana delivery</title>
+        <meta name="description" content="Estilo y sabor" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      homepage
+
+
+    </div>
+  );
+}
