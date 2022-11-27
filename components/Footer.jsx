26bd17814afe86b55d8c3f0ec393311a4cab07ef
Magdalena Ortega Solís <magdalena.ortegasolis@gmail.com>
2022-11-27 19:04:46 -0300

    slider creado

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,42 +10,32 @@ const Footer = () => {
       <div className={styles.item}>
         <div className={styles.card}>
           <h2 className={styles.motto}>
-            OH YES, WE DID.THE LAMA PIZZA, WELL BAKED SLICE OF PIZZA.
+            COMIDA Y ESTILO, TODO EN EL MISMO LUGAR
           </h2>
         </div>
         <div className={styles.card}>
-          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <h1 className={styles.title}>ESTAMOS MUY CERCA DE TI</h1>
           <p className={styles.text}>
-            1654 R. Don Road #304.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1010
+            <br /> NEW YORK
+            <br /> BIG AVENUE
           </p>
           <p className={styles.text}>
-            2356 K. Laquie Rd #235.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1011
+                      <br /> SANTIAGO
+            <br /> AVENIDA VITACURA
           </p>
           <p className={styles.text}>
-            1614 E. Erwin St #104.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1012
+                       <br /> SAO PAULO
+            <br /> AVENIDA BRASIL 
           </p>
-          <p className={styles.text}>
-            1614 W. Caroll St #125.
-            <br /> NewYork, 85022
-            <br /> (602) 867-1013
-          </p>
+          
         </div>
         <div className={styles.card}>
-          <h1 className={styles.title}>WORKING HOURS</h1>
+          <h1 className={styles.title}>ESTAMOS ABIERTOS</h1>
           <p className={styles.text}>
-            MONDAY UNTIL FRIDAY
-            <br /> 9:00 – 22:00
+          
+            <br />  ATENCIÓN 24 HORAS
           </p>
-          <p className={styles.text}>
-            SATURDAY - SUNDAY
-            <br /> 12:00 – 24:00
-          </p>
+         
         </div>
       </div>
     </div>
