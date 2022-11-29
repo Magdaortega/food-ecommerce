05591720ed90b1ca0d5857f279fc9a37c53dd95f
Magdalena Ortega Solís <magdalena.ortegasolis@gmail.com>
2022-11-29 19:21:31 -0300

    Product card agregado

---
+++ components/ProductList.jsx
@@ -0,0 +1,18 @@
+import styles from "../styles/ProductList.module"
+
+const ProductList = () => {
+    return (
+        <div className={styles.container}>
+
+            <h1 className={styles.title}>Comida saludable y con Estilo</h1>
+            <p className={styles.desc}>
+                lalalla
+            </p>
+            <div className={styles.wrapper}>
+
+            </div>
+        </div>
+
+    );
+};
+export default ProductList
